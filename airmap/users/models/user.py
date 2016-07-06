579ec7e0848ca5f0de81aa31579f4e082da0fc2c
wonju <gahuins@gmail.com>
2016-07-06 18:58:13 +0900

    refactor Makefile, users-model : add migrations airmpa & delete phonenumrber

--- airmap/users/models/user.py
+++ airmap/users/models/user.py
@@ -3,9 +3,4 @@ from custom_user.models import AbstractEmailUser
 
 
 class User(AbstractEmailUser):
-
-    phonenumber = models.CharField(
-        max_length=16,
-        blank=True,
-        null=True,
-    )
+    pass
