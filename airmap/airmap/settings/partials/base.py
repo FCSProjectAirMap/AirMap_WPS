62339f580efd656189430baa7071b5f91de96eae
lioliolio <kpc00100@gmail.com>
2016-07-13 18:10:21 +0900

    update settings: add Template DIR

--- airmap/airmap/settings/partials/base.py
+++ airmap/airmap/settings/partials/base.py
@@ -52,7 +52,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, "airmap", "templates"),    
+            os.path.join(BASE_DIR, "airmap", "templates"),
          ],
         'APP_DIRS': True,
         'OPTIONS': {
